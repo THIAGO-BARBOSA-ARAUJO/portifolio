1229b414d187f94e25860a8b51277c39c215f20e
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2023-03-10 17:23:52 -0300

    adicionei a rota projects no projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'skills', component: SkillsComponent}
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
