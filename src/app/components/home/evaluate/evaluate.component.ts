c61ca1d50b4a510f637647fb379abcb391d78fee
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2023-03-13 16:56:29 -0300

    Implementei a funcionalidade de deixar uma nota no meu perfil.

--- src/app/components/home/evaluate/evaluate.component.ts
+++ src/app/components/home/evaluate/evaluate.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-evaluate',
   templateUrl: './evaluate.component.html',
-  styleUrls: ['./evaluate.component.css']
+  styleUrls: ['./evaluate.component.css'],
 })
 export class EvaluateComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  star: string = '';
+
+  @Output() changeStar: EventEmitter<any> = new EventEmitter();
+
+  getInputStar(value: string) {
+    this.star = value;
   }
 
+  handleClick() {
+    this.changeStar.emit(this.star);
+  }
 }
